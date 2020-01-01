3e5b0c25c9f6fa204220fe36ccc6ec3e809fa192
17379 <173798637@qq.com>
2020-01-01 17:32:59 +0800

    native jdbc demo project.

---
+++ jdbc/src/test/java/com/taoxi/study/jdbc/JDBCTest.java
@@ -0,0 +1,16 @@
+package com.taoxi.study.jdbc;
+
+import org.junit.Test;
+
+public class JDBCTest {
+
+    @Test
+    public void testJDBC() throws Exception {
+
+        NativeJDBC.query();
+
+    }
+
+
+
+}
