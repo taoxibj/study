1ae03516f0d1403026f1bc2f50a48718ab847fee
17379 <173798637@qq.com>
2020-01-17 21:44:26 +0800

    新建手写mybatis工程

---
+++ mysimple/src/test/java/com/taoxi/mybatis/mysimple/MysimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taoxi.mybatis.mysimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
