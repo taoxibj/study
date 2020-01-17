ac4b52c41703ba28e3ac4f18b8b341ae937e648c
17379 <173798637@qq.com>
2020-01-17 21:45:59 +0800

    新建手写mybatis工程

---
+++ mysimple/src/main/java/com/taoxi/mybatis/mysimple/MysimpleApplication.java
@@ -0,0 +1,15 @@
+/*
+package com.taoxi.mybatis.mysimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysimpleApplication.class, args);
+    }
+
+}
+*/
